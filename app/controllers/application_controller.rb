666c28b1e07531189827e935c1dd33ab0593a656
Abel Kalum <chumbabel@gmail.com>
2020-07-15 00:45:22 +0000

    modify application controller, create views/application/home.erb file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,15 @@ require './config/environment'
 
 class ApplicationController < Sinatra::Base
 
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-  end
-
-  get "/" do
-    erb :welcome
-  end
+  configure do 
+        set :public_folder, 'public'
+        set :views, 'app/views'
+        enable :sessions
+        set :session_secret, "fastly"
+    end
+ 
+    get '/' do
+      erb :"/application/home"
+    end
 
 end
