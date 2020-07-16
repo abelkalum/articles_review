3e6c0eb8f6cf9ebab4da238ac5e6a49ddbbbaf4c
Abel Kalum <chumbabel@gmail.com>
2020-07-16 14:38:35 +0000

    modify db/seeds.rb, reseed the DB, modify views/welcome.erb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
-A1 = Article.create(title: "What Are Fintechs and How Can They Help Small Business?", author: "Paul Sullivan")
-A2 = Article.create(title: "How to Make Your Tech Last Longer", author: "Brian Chen")
-A3 = Article.create(title: "Strong cybersecurity strategy is no longer a luxury for small businesses", author: "Jim Risch")
+User.create(username: "John", password: "secret")
+User.create(username: "Alex", password: "secret")
+User.create(username: "William", password: "secret")  
+
+Article.create(title: "What Are Fintechs and How Can They Help Small Business?", author: "Paul Sullivan")  
+Article.create(title: "How to Make Your Tech Last Longer", author: "Brian Chen")
+Article.create(title: "Strong cybersecurity strategy is no longer a luxury for small businesses", author: "Jim Risch")
