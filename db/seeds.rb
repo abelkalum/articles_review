c4e1bdd5bd120baae60604d35eb2c655ea123dbb
Abel Kalum <chumbabel@gmail.com>
2020-07-17 20:40:27 +0000

    create DB migrations and populate DB with seeds.rb file

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+A1 = Article.create(title: "What Are Fintechs and How Can They Help Small Business?", author: "Paul Sullivan")
+A2 = Article.create(title: "How to Make Your Tech Last Longer", author: "Brian Chen")
+A3 = Article.create(title: "Strong cybersecurity strategy is no longer a luxury for small businesses", author: "Jim Risch")
