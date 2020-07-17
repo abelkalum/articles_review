fe0ebcd7220a85ee7220f86e1bdac10483228be4
Abel Kalum <chumbabel@gmail.com>
2020-07-17 19:09:16 +0000

    modify signup.erb, login.erb, views/users/show.erb, and users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,8 +22,8 @@ class UsersController < ApplicationController
  
   post '/users' do
     @user = User.new(params)
-    if user.save
-      session[:user_id] = user.id
+    if @user.save
+      session[:user_id] = @user.id
       redirect "/users/#{@user.id}"
     else
       redirect '/signup'
