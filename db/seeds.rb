ed63722687188b0adf9bbc1ea93f5c2a9a6d4d95
Abel Kalum <chumbabel@gmail.com>
2020-07-17 20:18:07 +0000

    delete seeds.rb file

--- db/seeds.rb
+++
@@ -1,3 +0,0 @@
-A1 = Article.create(title: "What Are Fintechs and How Can They Help Small Business?", author: "Paul Sullivan")
-A2 = Article.create(title: "How to Make Your Tech Last Longer", author: "Brian Chen")
-A3 = Article.create(title: "Strong cybersecurity strategy is no longer a luxury for small businesses", author: "Jim Risch")
