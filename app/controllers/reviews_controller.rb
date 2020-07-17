6d92b5dbde58b7509a0fa7a17125d04adaa496e7
Abel Kalum <chumbabel@gmail.com>
2020-07-17 18:25:58 +0000

    modify reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -11,9 +11,13 @@
   end
 
   post '/reviews' do
-    redirect_if_not_logged_in
-    @review = Review.create(user_id: @current_user.id, article_id: @article.id, text: params[:user][:review])
-    redirect "/reviews/#{@review.id}"
+    if params[:content] != ""
+      redirect_if_not_logged_in
+      @review = Review.create(text: params[:text], user_id: @current_user.id, article_id: @article.id)
+      redirect "/reviews/#{@review.id}"
+    else
+      redirect '/reviews/new'
+    end
   end
 
   get '/reviews/:id' do
@@ -27,18 +31,18 @@
     if authorized_to_edit?(@review)
       erb :'/reviews/edit'
     else
-      redirect "users/#{current_user.id}"
+      redirect "users/#{@current_user.id}"
     end
   end
 
   patch '/reviews/:id' do
     redirect_if_not_logged_in
     set_review
-    if @review.user == current_user && params[:user][:review] != ""
-      @review.update(content: params[:user][:review])
+    if @review.user == current_user && params[:text] != ""
+      @review.update(text: params[:text])
       redirect "/reviews/#{@review.id}"
     else
-      redirect "users/#{current_user.id}"
+      redirect "users/#{@current_user.id}"
     end
   end
 
